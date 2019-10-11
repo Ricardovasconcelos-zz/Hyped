7729bf94db74aa5bfec7a22500cf15e987e57cdd
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-10 23:59:43 -0300

    added webview

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,9 +2,10 @@
 import { createStackNavigator } from 'react-navigation'
 
 import Main from './pages/main'
-
+import Product from './pages/product'
 export default createStackNavigator({
-      Main
+      Main,
+      Product
 }, {
       navigationOptions:{
             headerStyle:{
